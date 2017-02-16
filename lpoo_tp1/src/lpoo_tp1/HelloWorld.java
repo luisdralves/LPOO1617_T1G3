9d7050d507a16262dbf3a8d9281386f9de62326a
gitinho <thug6757@gmail.com>
2017-02-16 15:00:12 +0000

    task #1

--- lpoo_tp1/src/lpoo_tp1/HelloWorld.java
+++ lpoo_tp1/src/lpoo_tp1/HelloWorld.java
@@ -3,7 +3,26 @@ package lpoo_tp1;
 public class HelloWorld
 {
 	public static void main(String[] args)
-	{ 
-		System.out.println("Hello, World"); 
+	{
+		Character board[][] = {{'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X'},
+							   {'X', 'H', ' ', ' ', 'I', ' ', 'X', ' ', 'G', 'X'},
+							   {'X', 'X', 'X', ' ', 'X', 'X', 'X', ' ', ' ', 'X'},
+							   {'X', ' ', 'I', ' ', 'I', ' ', 'X', ' ', ' ', 'X'},
+							   {'X', 'X', 'X', ' ', 'X', 'X', 'X', ' ', ' ', 'X'},
+							   {'I', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 'X'},
+							   {'I', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 'X'},
+							   {'X', 'X', 'X', ' ', 'X', 'X', 'X', 'X', ' ', 'X'},
+							   {'X', ' ', 'I', ' ', 'I', ' ', 'X', 'K', ' ', 'X'},
+							   {'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X'}
+		};
+		for (int i = 0; i < 10; i++)
+		{
+			for (int j = 0; j < 10; j++)
+			{
+				System.out.print(board[i][j]);
+				System.out.print(' ');
+			} 
+			System.out.print('\n');
+		} 
 	} 
 }
