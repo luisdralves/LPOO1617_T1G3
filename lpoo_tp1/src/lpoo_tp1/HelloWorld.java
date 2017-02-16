f3c5481b59d0235944f607cf28b099c7c769318e
gitinho <thug6757@gmail.com>
2017-02-16 15:20:33 +0000

    task #2 (hero movement)

--- lpoo_tp1/src/lpoo_tp1/HelloWorld.java
+++ lpoo_tp1/src/lpoo_tp1/HelloWorld.java
@@ -1,5 +1,7 @@
 package lpoo_tp1;
 
+import java.util.Scanner; 
+
 public class HelloWorld
 {
 	public static void main(String[] args)
@@ -15,6 +17,13 @@ public class HelloWorld
 							   {'X', ' ', 'I', ' ', 'I', ' ', 'X', 'K', ' ', 'X'},
 							   {'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X'}
 		};
+		
+		Scanner input = new Scanner(System.in);
+		int heroX = 1, heroY = 1;
+		char move;
+		boolean gameNotOver = true;
+		
+		
 		for (int i = 0; i < 10; i++)
 		{
 			for (int j = 0; j < 10; j++)
@@ -23,6 +32,36 @@ public class HelloWorld
 				System.out.print(' ');
 			} 
 			System.out.print('\n');
-		} 
+		}
+		
+		while (gameNotOver)
+		{
+			System.out.print("Direction of movement (wasd):");
+			move = input.next().charAt(0);
+			board[heroY][heroX] = ' ';
+			switch (move) {
+			case 'w':
+				heroY--;
+				board[heroY][heroX] = 'H';
+			case 's':
+				heroY++;
+				board[heroY][heroX] = 'H';
+			case 'a':
+				heroX--;
+				board[heroY][heroX] = 'H';
+			case 'd':
+				heroX++;
+				board[heroY][heroX] = 'H';
+			}
+			for (int i = 0; i < 10; i++)
+			{
+				for (int j = 0; j < 10; j++)
+				{
+					System.out.print(board[i][j]);
+					System.out.print(' ');
+				} 
+				System.out.print('\n');
+			}
+		}
 	} 
 }
