f97b3a6e7642fc5ab405894eab5776e2cc326d56
gitinho <thug6757@gmail.com>
2017-02-16 15:54:03 +0000

    task #2 (complete)

--- lpoo_tp1/src/lpoo_tp1/HelloWorld.java
+++ lpoo_tp1/src/lpoo_tp1/HelloWorld.java
@@ -20,6 +20,9 @@ public class HelloWorld
 		
 		Scanner input = new Scanner(System.in);
 		int heroX = 1, heroY = 1;
+		int leverX = 7, leverY = 8;
+		int exitX = 0, exit1Y = 5, exit2Y = 6;
+		int guardX = 8, guardY = 1;
 		char move;
 		boolean gameNotOver = true;
 		
@@ -42,17 +45,45 @@ public class HelloWorld
 			switch (move) {
 			case 'w':
 				heroY--;
-				board[heroY][heroX] = 'H';
+				break;
 			case 's':
 				heroY++;
-				board[heroY][heroX] = 'H';
+				break;
 			case 'a':
 				heroX--;
-				board[heroY][heroX] = 'H';
+				break;
 			case 'd':
 				heroX++;
-				board[heroY][heroX] = 'H';
+				break;
 			}
+			if (board[heroY][heroX] == 'X' || board[heroY][heroX] == 'I')
+			{
+				switch (move) {
+				case 'w':
+					heroY++;
+					break;
+				case 's':
+					heroY--;
+					break;
+				case 'a':
+					heroX++;
+					break;
+				case 'd':
+					heroX--;
+					break;
+				}
+				board[heroY][heroX] = 'H';
+				System.out.println("You're not a ghost");
+			}
+			else
+				board[heroY][heroX] = 'H';
+			
+			if (heroX == leverX && heroY == leverY)
+			{
+				board[exit1Y][exitX] = 'S';
+				board[exit2Y][exitX] = 'S';
+			}
+			
 			for (int i = 0; i < 10; i++)
 			{
 				for (int j = 0; j < 10; j++)
@@ -61,6 +92,18 @@ public class HelloWorld
 					System.out.print(' ');
 				} 
 				System.out.print('\n');
+			}
+			
+			if (heroX == exitX && (heroY == exit1Y || heroY == exit2Y))
+			{
+				System.out.println("You win");
+				gameNotOver = false;
+			}
+			
+			if ((heroX == guardX && (heroY == guardY-1 || heroY == guardY+1)) || (heroY == guardY && (heroX == guardX-1 || heroX == guardX+1)))
+			{
+				System.out.println("Caught by the guard");
+				gameNotOver = false;
 			}
 		}
 	} 
