b38874258d3d53db12bae307ecfb39c988a0e6f2
gitinho <thug6757@gmail.com>
2017-05-25 15:22:58 +0100

    first libgdx commit

---
+++ libgdx_ver/Monopoly/desktop/src/com/lpoo1617t1g3/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.lpoo1617t1g3.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.lpoo1617t1g3.Monopoly;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Monopoly(), config);
+	}
+}
---
+++ libgdx_ver/Monopoly/html/src/com/lpoo1617t1g3/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package com.lpoo1617t1g3.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.lpoo1617t1g3.Monopoly;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener createApplicationListener () {
+                return new Monopoly();
+        }
+}
