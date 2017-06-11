e8f9b5da6d4ccd5e4e9ddee7ab24062d5bc88b24
P1r3s <mrpires92@gmail.com>
2017-06-12 00:16:36 +0100

    Updated textures and buttons

--- Monopoly/core/src/screens/NewGameScreen.java
+++ Monopoly/core/src/screens/NewGameScreen.java
@@ -6,6 +6,7 @@ import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.Camera;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.InputListener;
 import com.badlogic.gdx.scenes.scene2d.Stage;
@@ -36,6 +37,7 @@ public class NewGameScreen implements Screen {
     private TextButton btnPlay, btnBack;
     private TextField playerCountField;
     private ImageButton playerCountMore, playerCountLess;
+    private Texture background;
 
     public NewGameScreen(Monopoly m) {
         game = m;
@@ -51,6 +53,8 @@ public class NewGameScreen implements Screen {
 
         initActors();
         initTable();
+
+        background = new Texture("newgame_bg.jpg");
     }
 
     private void initActors() {
@@ -153,7 +157,9 @@ public class NewGameScreen implements Screen {
     public void render(float delta) {
         Gdx.gl.glClearColor(1, 1, 1, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-
+        game.spb.begin();
+        game.spb.draw(background, 0, 0, Monopoly.WIDTH, Monopoly.HEIGHT);
+        game.spb.end();
         stage.act(delta);
         stage.draw();
     }
